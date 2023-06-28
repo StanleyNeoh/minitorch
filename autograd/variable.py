Fixes minibatch backprop bug
Broadcasting is dangerous, and I was doing it wrong. This fixes it.
I don't know how to describe the problem in words and notation.

autograd/variable.py
@@ -171,6 +171,16 @@ class V:
         Add a gradient to this variable.
         This is used to accumulate gradient from multiple sources.
         If this variable does not require gradient, do nothing.
+       
+        A few cases to consider:
+        * grad is broadcastable to self.data.shape
+        * self.data.shape is broadcastable to grad.shape
+            * This can happen when multiple samples are used.
+            * grad.shape will be (batch_size, *self.data.shape)
+            * To ensure further flexibility, we wil generalise incoming grad to have shape (..., *self.data.shape).
+            * We cannot have self.grad broadcast to grad.shape as this will result in self.grad having shape (..., *self.data.shape)
+                which is not what we want.
+            * The way to solve this is to sum over all dimensions except the last self.data.ndim dimensions.
 
         Args:
             grad (np.ndarray): gradient to be added
@@ -178,7 +188,9 @@ class V:
             None
         """
         if self.requires_grad:
-            self.grad = self.grad + grad
+            if grad.ndim > self.data.ndim:
+                grad = np.sum(grad, axis=tuple(range(grad.ndim - self.data.ndim)))
+            self.grad += grad
     
     def add_deps(self, vars: list[V]) -> None:
         """

optimiser/optimiser.py
@@ -20,7 +20,7 @@ class Optimiser:
         return str(self.__repr__())
 
 class SGD(Optimiser):
-    def __init__(self, *parameters: V, lr: float = 0.001):
+    def __init__(self, *parameters: V, lr: float = 0.01):
         super().__init__(*parameters)
         self.lr = lr
     
@@ -29,7 +29,7 @@ class SGD(Optimiser):
             parameter.data = parameter.data - (parameter.grad * self.lr)
 
 class Adagrad(Optimiser):
-    def __init__(self, *parameters: V, lr: float = 0.001, eps: float = 1e-8):
+    def __init__(self, *parameters: V, lr: float = 0.01, eps: float = 1e-8):
         super().__init__(*parameters)
         self.lr = lr
         self.eps = eps
@@ -41,7 +41,7 @@ class Adagrad(Optimiser):
             parameter.data = parameter.data - (self.lr * parameter.grad / (np.sqrt(cache) + self.eps))
 
 class Adam(Optimiser):
-    def __init__(self, *parameters: V, lr: float = 0.001, beta1: float = 0.9, beta2: float = 0.999, eps: float = 1e-8):
+    def __init__(self, *parameters: V, lr: float = 0.01, beta1: float = 0.9, beta2: float = 0.999, eps: float = 1e-8):
         super().__init__(*parameters)
         self.lr = lr
         self.beta1 = beta1