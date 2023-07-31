Fixes disconnect bug in softmax

autograd/functions.py
@@ -132,7 +132,7 @@ def softmax(var: V, axis=None) -> V:
     data /= np.sum(data, axis=axis, keepdims=True)
     out = V(data, requires_grad=require_grad)
     def _backward():
-        out.add_to_grad(out.grad * data * (1 - data))
+        var.add_to_grad(out.grad * data * (1 - data))
     out.set_backward(_backward)
     out.add_deps([var])
     return out

autograd/losses.py
@@ -154,7 +154,7 @@ def huberloss(output: V, target: V, delta: float = 1.0, axis=None, keepdims=Fals
     return loss
 
 
-def crossentropyloss(output: V, target_i: np.ndarray, axis=None, keepdims=False) -> V:
+def crossentropyloss(output: V, target_i: np.ndarray, eps=1e-5) -> V:
     """
     Cross entropy loss for a variable.
 
@@ -184,7 +184,7 @@ def crossentropyloss(output: V, target_i: np.ndarray, axis=None, keepdims=False)
         if output.requires_grad:
             slicer = (np.arange(num_batches), target_i)
             masked = np.zeros(output.data.shape)
-            masked[*slicer] = (-1.0 / (num_batches * output.data))[*slicer]
+            masked[*slicer] = (-1.0 / (num_batches * output.data + eps))[*slicer]
             output.add_to_grad(masked * loss.grad)
 
     loss.set_backward(_backward)

model/layer.py
@@ -90,7 +90,7 @@ class Sequential(Layer):
         """
         parameters = []
         for layer in self.layers:
-            parameters += layer.parameters()
+            parameters.extend(layer.parameters())
         return parameters
 
     def copy(self, reinitialise: bool=False) -> Layer:
@@ -245,7 +245,45 @@ class Sigmoid(Layer):
             Layer: Copy of the Sigmoid layer.
         """
         return Sigmoid()
+
+class Softmax(Layer):
+    def __init__(self, axis: int = -1) -> None:
+        self.axis = axis
+
+    def forward(self, input) -> V:
+        """
+        Forward pass of the Softmax layer.
+
+        Args:
+            input (V): Input to the Softmax layer.
+
+        Returns:
+            V: Output of the Softmax layer.
+        """
+        output = F.softmax(input, axis=self.axis)
+        return output
     
+    def parameters(self):
+        """
+        Get the parameters of the Softmax layer.
+
+        Returns:
+            list[V]: Parameters of the Softmax layer.
+        """
+        return []
+
+    def copy(self, reinitialise: bool=False) -> Layer:
+        """
+        Copy the Softmax layer.
+
+        Args:
+            reinitialise (bool, optional): This parameter is ignored. Defaults to False.
+            
+        Returns:
+            Layer: Copy of the Softmax layer.
+        """
+        return Softmax(axis=self.axis)
+
 
 class Ly:
     """
@@ -256,3 +294,4 @@ class Ly:
     Linear = Linear
     ReLU = ReLU
     Sigmoid = Sigmoid
+    Softmax = Softmax